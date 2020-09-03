9187ce900e4113d454f4c141e13715c6eea6dc22
arnardecay <arnarj19@ru.is>
2020-09-03 10:04:57 +0000

    Algorithm written for max_in

---
+++ max_int.py
@@ -0,0 +1,15 @@
+
+num_int = int(input("Input a number: ")) # Do not change this line
+max_int = num_int
+# Fill in the missing code
+while num_int > 0:
+    num_int = int(input("Input a number: "))
+    if num_int < 0:
+        break
+    elif num_int > num_int:
+        print(num_int)
+    
+        
+    
+
+print("The maximum is", max_int)    # Do not change this line
